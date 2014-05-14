8b6fae0e6c24b7daeea1aabbfbf77b7184bc130d
Jefferson Xavier <jeffer14.souza@hotmail.com>
2014-05-14 12:34:53 -0300

    init test cases

---
+++ AcademicSystem/src/academicSystem/tests/jmock/course/CourseTest.java
@@ -0,0 +1,81 @@
+package academicSystem.tests.jmock.course;
+
+import static org.junit.Assert.*;
+
+import org.jmock.Mockery;
+import org.junit.Test;
+
+public class CourseTest {
+	Mockery context = new Mockery();
+
+	@Test
+	public void testCourse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCredits() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCredits() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPeriods() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPeriods() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStudents() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddStudent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDisciplines() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddDiscipline() {
+		fail("Not yet implemented");
+	}
+
+}
